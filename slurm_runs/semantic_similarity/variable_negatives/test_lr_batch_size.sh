0c7ffa38b7f91d548e34c04bf4794caf48298c0c
Andrew Moore <andrew.p.moore94@gmail.com>
2025-08-20 13:18:25 +0000

    Small argument adjustment

--- slurm_runs/semantic_similarity/variable_negatives/test_lr_batch_size.sh
+++ slurm_runs/semantic_similarity/variable_negatives/test_lr_batch_size.sh
@@ -4,6 +4,8 @@
 #SBATCH --error=log/error/semantic_similarity_variable_negatives_test_lr_batch_size.log
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus=1
+#SBATCH --ntasks-per-node=1
+
 
 srun python $(pwd)/training_runs/semantic_similarity/get_token_similarity_variables_negatives_lr_batch_sizes.py \
 --config training_runs/semantic_similarity/variable_negatives_configs/base_config.yaml \
